361219952736ea55f4ba561c5f7e8d6801da4af2
hiroki <hiroki22991395@yahoo.co.jp>
2016-12-02 13:53:03 +0900

    change main.go

--- main.go
+++ main.go
@@ -3,23 +3,65 @@ package main
 import (
     "log"
     "os"
+    "encoding/json"
+    "io/ioutil"
+    "net/http"
+    "time"
+    "regexp"
     "github.com/gin-gonic/gin"
     "github.com/PuerkitoBio/goquery"
     "github.com/line/line-bot-sdk-go/linebot"
 )
 
-func ScrapeText(url string) string{
-    res, err := goquery.NewDocument(url)
+type TrainInfomation struct {
+    Context                string    `json:"@context"`
+    Id                     string    `json:"@id"`
+    Type                   string    `json:"@type"`
+    Date                   time.Time `json:"dc:date"`
+    Valid                  time.Time `json:"dct:valid"`
+    Operator               string    `json:"odpt:operator"`
+    TimeOfOrigin           time.Time `json:"odpt:timeOfOrigin"`
+    Railway                string    `json:"odpt:railway"`
+    TrainInformationStatus string    `json:"odpt:trainInformationStatus"`
+    TrainInformationText   string    `json:"odpt:trainInformationText"`
+}
+
+type TrainInformations []TrainInfomation
+
+func FetchTrainInfo(message string) string{
+    url := make([]byte, 0, 10)
+    url = append(url, "https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:TrainInformation&acl:consumerKey="...)
+    url = append(url, os.Getenv("CONSUMER_KEY")...)
+
+    res, err := http.Get(string(url))
     if err != nil {
-        log.Print(err)
+        log.Fatal(err)
     }
-    title, _ := res.Find("h2").Html()
-    text := make([]byte, 0, 10)
-    text = append(text, title...)
-    res.Find(".kcm-read-text > p").Each(func(_ int, s *goquery.Selection) {
-        // text = append(text, s.Text()...)
-    })
-    return string(text)
+
+    defer res.Body.Close()
+
+    body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    var trains TrainInformations
+    err = json.Unmarshal(body, &trains)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, train := range trains {
+        rep := regexp.MustCompile(`[A-Za-z]*odpt.Railway:TokyoMetro.`)
+        text := make([]byte, 0, 10)
+        text = rep.ReplaceAllString(train.Railway, "")
+        text = append(text, train.TrainInformationText...)
+        if len(train.TrainInformationStatus) > 0 {
+            text = append(text, train.TrainInformationStatus...)
+        }
+        message += srting(text)
+    }
+    return message
 }
 
 func main() {
@@ -50,15 +92,10 @@ func main() {
         }
         for _, event := range events {
             if event.Type == linebot.EventTypeMessage {
-                switch event.Message.(type) {
+                switch message := event.Message.(type) {
                 case *linebot.TextMessage:
-                    res, err := goquery.NewDocument("http://news.kompas.com/")
-                    if err != nil {
-                        log.Print(err)
-                    }
-                    url, _ := res.Find(".list-most > a").First().Attr("href")
-                    news := ScrapeText(url)
-                    if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(news)).Do(); err != nil {
+                    text := FetchTrainInfo(message)
+                    if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(text)).Do(); err != nil {
                         log.Print(err)
                     }
                 }
